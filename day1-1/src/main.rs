2ea561a85e93b4fd4dd27a3b33e756c0f192baa9
Justin <justin@jscd.pw>
2020-12-01 12:50:17 -0500

    Add day 1 part 1 solution

---
+++ day1-1/src/main.rs
@@ -0,0 +1,27 @@
+// Advent of Code — Day 1 Problem 1 Solution
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const INPUT_PATH: &str = "day1-1.input";
+
+fn main() {
+  let input = File::open(INPUT_PATH).unwrap();
+  let buffered = BufReader::new(input);
+
+  let mut nums: Vec<u32> = Vec::new();
+
+  for line in buffered.lines() {
+    nums.push(line.unwrap().parse::<u32>().unwrap())
+  }
+
+
+  for num1 in &nums {
+    for num2 in &nums {
+      if (num1 != num2) && (num1 + num2 == 2020) {
+        println!("{}", num1*num2);
+        return;
+      }
+    }
+  }
+}
